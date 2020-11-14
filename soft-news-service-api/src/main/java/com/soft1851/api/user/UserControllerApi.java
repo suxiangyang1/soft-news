58505d3103907487f1362d4adeb9f6cc36377a2a
杨苏祥 <3597107837@qq.com>
2020-11-15 00:40:20 +0800

    Swagger2接口文档工具的使用

--- soft-news-service-api/src/main/java/com/soft1851/api/user/UserControllerApi.java
+++ soft-news-service-api/src/main/java/com/soft1851/api/user/UserControllerApi.java
@@ -1,17 +1,34 @@
 package com.soft1851.api.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.soft1851.result.GraceResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author ysx
  */
-
+@Api(value = "用户相关Controller",tags = {"用户相关Controller"})
+@RequestMapping("user")
 public interface UserControllerApi {
 
     /**
      * 获取所有用户
      * @return
      */
-    @GetMapping("/users")
-    Object getUsers();
+    @ApiOperation(value = "获取用户信息",notes = "获得用户信息",httpMethod = "POST")
+    @PostMapping("/all")
+    GraceResult getAllUsers();
+
+
+    /**
+     * 获取用户基本信息
+     * @param userId
+     * @return 用户基本信息
+     */
+    @ApiOperation(value = "获得用户基本信息",notes = "获得用户基本信息",httpMethod = "POST")
+    @PostMapping("/userInfo")
+    GraceResult getUserInfo(@RequestParam String userId);
 }
--- soft-news-service-user/src/main/java/com/soft1851/user/controller/UserController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.user.UserControllerApi;
+import com.soft1851.pojo.AppUser;
 import com.soft1851.result.GraceResult;
 import com.soft1851.user.mapper.AppUserMapper;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 
@@ -16,7 +18,16 @@ public class UserController implements UserControllerApi {
     private AppUserMapper appUserMapper;
 
     @Override
-    public Object getUsers() {
+    public GraceResult getAllUsers() {
         return GraceResult.ok(appUserMapper.selectAll());
+    }
+
+    @Override
+    public GraceResult getUserInfo(String userId) {
+        Example useExample = new Example(AppUser.class);
+        Example.Criteria userCriteria = useExample.createCriteria();
+        userCriteria.andEqualTo("id",userId);
+        AppUser user = appUserMapper.selectOneByExample(useExample);
+        return GraceResult.ok(user);
     }
 }
